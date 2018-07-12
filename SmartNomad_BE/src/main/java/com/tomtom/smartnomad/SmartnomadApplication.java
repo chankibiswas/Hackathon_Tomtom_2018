829a4584beaa7c4e160cbec456a8781a9014ffea
Chanki Biswas <chanki.biswas@tomtom.com>
2018-07-12 14:32:46 +0530

    Backend project structure created

---
+++ SmartNomad_BE/src/main/java/com/tomtom/smartnomad/SmartnomadApplication.java
@@ -0,0 +1,12 @@
+package com.tomtom.smartnomad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartnomadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartnomadApplication.class, args);
+    }
+}
